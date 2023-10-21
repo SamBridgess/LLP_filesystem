690e11ed262be53b287d04896bbfa22b8c732ff7
Ususi <79852601706@yandex.ru>
2023-10-21 18:54:04 +0300

    add fs structs

--- llp_fs.c
+++ llp_fs.c
@@ -1,181 +1,75 @@
-#define FUSE_USE_VERSION 30
+#define FUSE_USE_VERSION 26
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef linux
+/* For pread()/pwrite() */
+#define _XOPEN_SOURCE 500
+#endif
+
+#define _GNU_SOURCE
 
 #include <fuse.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <time.h>
-#include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
 #include <errno.h>
+#include <sys/time.h>
+#include <assert.h>
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
 
-char dir_list[ 256 ][ 256 ];
-int curr_dir_idx = -1;
+#define FAT_DISK_SIZE 10485760 // 10 MB = 10485760 B
+#define FAT_DISK_NAME "fat_disk"
 
-char files_list[ 256 ][ 256 ];
-int curr_file_idx = -1;
+#define DIR_ENTRY_MAX_NAME_LEN 21
+#define DIR_ENTRY_TYPE_FILE 0
+#define DIR_ENTRY_TYPE_SUBDIR 1
+#define DIR_ENTRY_TYPE_SYMLINK 2
 
-char files_content[ 256 ][ 256 ];
-int curr_file_content_idx = -1;
+#define BYTE_PER_CLUSTER 4096
+#define CLUSTER_COUNT 2560
+#define CLUSTER_PER_FAT 3
+#define FAT_TYPE 32
+#define ROOT_CLUSTER_START 4
+#define FREE_CLUSTER_START 5
 
-void add_dir( const char *dir_name )
-{
-	/*
-	curr_dir_idx++;
-	strcpy( dir_list[ curr_dir_idx ], dir_name );
-	*/
-}
+#define END_OF_CHAIN 0
 
-int is_dir( const char *path )
-{
-	path++; // Eliminating "/" in the path
-	
-	for ( int curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++ )
-		if ( strcmp( path, dir_list[ curr_idx ] ) == 0 )
-			return 1;
-	
-	return 0;
-}
-
-void add_file( const char *filename )
-{
-	curr_file_idx++;
-	strcpy( files_list[ curr_file_idx ], filename );
-	
-	curr_file_content_idx++;
-	strcpy( files_content[ curr_file_content_idx ], "" );
-}
-
-int is_file( const char *path )
-{
-	path++; // Eliminating "/" in the path
-	
-	for ( int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++ )
-		if ( strcmp( path, files_list[ curr_idx ] ) == 0 )
-			return 1;
-	
-	return 0;
-}
-
-int get_file_index( const char *path )
-{
-	path++; // Eliminating "/" in the path
-	
-	for ( int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++ )
-		if ( strcmp( path, files_list[ curr_idx ] ) == 0 )
-			return curr_idx;
-	
-	return -1;
-}
-
-void write_to_file( const char *path, const char *new_content )
-{
-	int file_idx = get_file_index( path );
-	
-	if ( file_idx == -1 ) // No such file
-		return;
-		
-	strcpy( files_content[ file_idx ], new_content ); 
-}
-
-static int do_getattr( const char *path, struct stat *st )
-{
-	st->st_uid = getuid(); // The owner of the file/directory is the user who mounted the filesystem
-	st->st_gid = getgid(); // The group of the file/directory is the same as the group of the user who mounted the filesystem
-	st->st_atime = time( NULL ); // The last "a"ccess of the file/directory is right now
-	st->st_mtime = time( NULL ); // The last "m"odification of the file/directory is right now
-	
-	if ( strcmp( path, "/" ) == 0 || is_dir( path ) == 1 )
-	{
-		st->st_mode = S_IFDIR | 0755;
-		st->st_nlink = 2; // Why "two" hardlinks instead of "one"? The answer is here: http://unix.stackexchange.com/a/101536
-	}
-	else if ( is_file( path ) == 1 )
-	{
-		st->st_mode = S_IFREG | 0644;
-		st->st_nlink = 1;
-		st->st_size = 1024;
-	}
-	else
-	{
-		return -ENOENT;
-	}
-	
-	return 0;
-}
-
-static int do_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi )
-{
-	filler( buffer, ".", NULL, 0 ); // Current Directory
-	filler( buffer, "..", NULL, 0 ); // Parent Directory
-	
-	if ( strcmp( path, "/" ) == 0 ) // If the user is trying to show the files/directories of the root directory show the following
-	{
-		for ( int curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++ )
-			filler( buffer, dir_list[ curr_idx ], NULL, 0 );
-	
-		for ( int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++ )
-			filler( buffer, files_list[ curr_idx ], NULL, 0 );
-	}
-	
-	return 0;
-}
-
-static int do_read( const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi )
-{
-	int file_idx = get_file_index( path );
-	
-	if ( file_idx == -1 )
-		return -1;
-	
-	char *content = files_content[ file_idx ];
-	
-	memcpy( buffer, content + offset, size );
-		
-	return strlen( content ) - offset;
-}
-
-static int do_mkdir( const char *path, mode_t mode )
-{
-	path++;
-	add_dir( path );
-	
-	return 0;
-}
-
-static int do_mknod( const char *path, mode_t mode, dev_t rdev )
-{
-	path++;
-	add_file( path );
-	
-	return 0;
-}
-
-static int do_write( const char *path, const char *buffer, size_t size, off_t offset, struct fuse_file_info *info )
-{
-	write_to_file( path, buffer );
-	
-	return size;
-}
-static int do_unlink (const char *path)
-{
-	return 0;
-}
-
-
-
-
-static struct fuse_operations operations = {
-    .getattr	= do_getattr,
-    .readdir	= do_readdir,
-    .read		= do_read,
-    .mkdir		= do_mkdir,
-    .mknod		= do_mknod,
-    .write		= do_write,
-	.unlink  	= do_unlink,
+struct fat_superblock {
+    unsigned int byte_per_cluster;
+    unsigned int cluster_count;
+    unsigned int cluster_per_fat;
+    unsigned int fat_type;
+    unsigned int  root_cluster_start;
+    unsigned int free_cluster_start;
 };
 
-int main( int argc, char *argv[] )
-{
-	return fuse_main( argc, argv, &operations, NULL );
+union {
+    struct fat_superblock s;
+    char pad[512];
+} superblock;
+
+struct fat_dir_entry {
+    char name[DIR_ENTRY_MAX_NAME_LEN + 1];
+    unsigned short int type;
+    unsigned int size;
+    unsigned int first_cluster;
+};
+
+unsigned int DIR_ENTRY_SIZE = sizeof(struct fat_dir_entry);
+
+char *fat_table = NULL;
+char *fat_disk_path = NULL;
+FILE *fat_disk = NULL;
+
+unsigned int min(unsigned int a, unsigned int b) {
+    return (a < b) ? a : b;
 }
