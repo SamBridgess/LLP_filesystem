92857d2fa278f4ca8b848a41167b4f064dd78ff1
SamBridgess <ilya.minyaeff@yandex.ru>
2023-11-23 20:54:03 +0300

    server template cd fix

--- server_app/command_processor.c
+++ server_app/command_processor.c
@@ -1,44 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 int main() {
-    //char directory[1000] = "../fs/mount_test";
     chdir("../fs/mount_test");
-
 
     while(1) {
         char command[1000];
         char directory[1000] = ".";
-        // Get the directory path
-       //  printf("Enter the directory path: ");
-       // scanf("%s", directory);
 
         // Get the command
         printf("Enter the command: ");
         scanf(" %[^\n]s", command);
 
 
-    
-
-  // Change to the specified directory
-    if(command[0] == 'c' && command[1] == 'd' && command[2] == ' ') {
-            for (size_t i = 0; i <= strlen(command) - 3; i++) {
+        // Change to the specified directory
+        if(command[0] == 'c' && command[1] == 'd' && command[2] == ' ') {
+            int len = strlen(command);
+            for (size_t i = 0; i <= len - 3; i++) {
                 directory[i] = command[i + 3];
             }
-        
-        if (chdir(directory) != 0) {
-            perror("chdir");
-            exit(EXIT_FAILURE);
-        }
-        }
-        // Execute the command in the specified directory
-        if (system(command) == -1) {
-            perror("system");
-            exit(EXIT_FAILURE);
+            directory[len - 1] = '\0';
+
+            if (chdir(directory) != 0) {
+                perror("chdir");
+                //exit(EXIT_FAILURE);
+            }
+        } else {
+            // Execute the command in the specified directory
+            if (system(command) == -1) {
+                perror("system");
+                //exit(EXIT_FAILURE);
+            }
         }
     }
-    
-    
-
     return 0;
 }
